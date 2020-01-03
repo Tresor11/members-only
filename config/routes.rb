95eb5231c624bc633ed159b4c0c631cb6970c110
Tresor11 <tresor.moise2001@gmail.com>
2020-01-03 13:17:03 +0200

    added the styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'posts#index'
   resources :users
   resources :posts, only: %i[new create index show]
   get '/signup', to: 'users#new'

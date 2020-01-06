547557faaf8359a8faa19877fc9da70f7de033eb
Tresor11 <tresor.moise2001@gmail.com>
2020-01-06 19:01:29 +0200

    fixed more lint errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :posts
   attr_accessor :remember_token
   before_create :generate_token, :downcase_email
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
